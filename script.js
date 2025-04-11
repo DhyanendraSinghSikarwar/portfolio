05a47bd933e549381afd4bd7cb252d881aa4c4a1
Dhyanendra <dhyanendra@kreditz.com>
2025-04-11 13:42:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
+  });
+
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $("header").removeClass("toggle");
+
+    if ($(window).scrollTop() > 0) {
+      $(".top").show();
+    } else {
+      $(".top").hide();
+    }
+  });
+
+  // smooth scrolling
+
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
+    );
+  });
+});
+
+function updateBodyField() {
+  const name = document.querySelector('input[name="name"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  const message = document.querySelector('textarea[name="message"]').value;
+  
+  const body = `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`;
+  document.querySelector('input[name="body"]').value = body;
+}
+
+// document.addEventListener("contextmenu", (event) => {
+//   event.preventDefault();
+// });
+
+// Add smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Add form validation
+function validateForm() {
+    const name = document.querySelector('input[name="name"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const message = document.querySelector('textarea[name="message"]').value;
+
+    if (!name || !email || !message) {
+        alert('Please fill in all fields');
+        return false;
+    }
+
+    if (!email.includes('@')) {
+        alert('Please enter a valid email address');
+        return false;
+    }
+
+    return true;
+}
